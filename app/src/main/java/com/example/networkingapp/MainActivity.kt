32292bb7cecf293d4e89e60ca39708795aedef67
Sasha Riaboshapka <sashariaboshapka2003@gmail.com>
2022-08-23 15:13:13 +0300

    implemented MVVM structure

--- app/src/main/java/com/example/networkingapp/MainActivity.kt
+++ app/src/main/java/com/example/networkingapp/MainActivity.kt
@@ -1,20 +1,11 @@
 package com.example.networkingapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.core.R
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
-import androidx.lifecycle.lifecycleScope
 import com.example.networkingapp.databinding.ActivityMainBinding
-import com.example.networkingapp.userAPI.RetrofitInstance
-import com.example.networkingapp.userAPI.model.RandomUserAPIResponse
-import retrofit2.Call
-import retrofit2.Callback
-
-import retrofit2.HttpException
-import retrofit2.Response
-import java.io.IOException
 
 const val TAG = "MyApp"
 
@@ -23,6 +14,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var usersListAdapter: UsersListAdapter
 
+    private val viewModel:MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -30,47 +23,24 @@ class MainActivity : AppCompatActivity() {
 
         setupUsersList()
 
+        binding.requestButton.setOnClickListener {
+            loadUsers()
+        }
+
+    }
+
+    private fun loadUsers() {
+        viewModel.fetchDataFromRepo()
+
         binding.progressBar.isVisible = true
-        RetrofitInstance.api.getRandomUserAPIResponse(2,3).enqueue(object : Callback<RandomUserAPIResponse>{
-            override fun onResponse(
-                call: Call<RandomUserAPIResponse>,
-                response: Response<RandomUserAPIResponse>
-            ) {
+        viewModel.userAPIResponseResult.observe(this) { result ->
+            if (result.isSuccess && result != null) {
+                usersListAdapter.users = result.getOrNull()!!.results
                 binding.progressBar.isVisible = false
-                if (response.isSuccessful && response.body() != null)
-                    usersListAdapter.users = response.body()!!.results
+            } else {
+                Log.e(TAG, "observer: ${result.exceptionOrNull()?.printStackTrace()}")
             }
-
-            override fun onFailure(call: Call<RandomUserAPIResponse>, t: Throwable) {
-                t.printStackTrace()
-            }
-
-        })
-
-        /*lifecycleScope.launchWhenCreated {
-            binding.progressBar.isVisible = true
-
-            val response = try {
-                RetrofitInstance.api.getRandomUserAPIResponse(2,3).execute()
-            } catch (e: IOException){
-                Log.e(TAG, "IOException: check your internet connection" )
-                binding.progressBar.isVisible = false
-                return@launchWhenCreated
-            }catch (e: HttpException){
-                Log.e(TAG, "HttpException: unexpected response" )
-                binding.progressBar.isVisible = false
-                return@launchWhenCreated
-            }
-
-
-            if (response.isSuccessful && response.body() != null){
-                usersListAdapter.users = response.body()!!.results
-            }else{
-                Log.e(TAG, "Response not successful" )
-            }
-
-            binding.progressBar.isVisible = false
-        }*/
+        }
     }
 
     private fun setupUsersList() = binding.usersRV.apply {
---
+++ app/src/main/java/com/example/networkingapp/MainViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.networkingapp
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.networkingapp.repository.MainRepository
+import com.example.networkingapp.userAPI.model.RandomUserAPIResponse
+
+class MainViewModel : ViewModel(), MainRepository.ProcessRequestResult {
+
+    private val repository get() =  MainRepository(this)
+
+    private val _userAPIResponseResult = MutableLiveData<Result<RandomUserAPIResponse>>()
+
+    val userAPIResponseResult: MutableLiveData<Result<RandomUserAPIResponse>>
+        get() = _userAPIResponseResult
+
+    fun fetchDataFromRepo(){
+        repository.makeUsersAPIQuery()
+    }
+
+    override fun process(result: Result<RandomUserAPIResponse>) {
+        _userAPIResponseResult.value = result
+    }
+}
